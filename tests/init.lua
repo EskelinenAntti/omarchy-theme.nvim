928214a9a7e74aeaf6c716e456535d19f47957d8
Antti Eskelinen <antti.eskelinen.dev@gmail.com>
2026-09-05 09:43:32 +0300

    test: add test setup (#9)
    Adds minimal test setup:
    - Small Lua util for running all test functions under tests folder
    - Run the tests against actual Neovim API with headless Neovim instance
    - No 3rd party plugins

---
+++ tests/init.lua
@@ -0,0 +1,27 @@
+local function get_test_files()
+	local files = {}
+
+	for name, type in vim.fs.dir("tests") do
+		if type == "file" and name:match("^test.*%.lua$") then
+			table.insert(files, name)
+		end
+	end
+
+	return files
+end
+
+for _, file in ipairs(get_test_files()) do
+	local module = "tests." .. file:gsub("%.lua$", "")
+	local tests = require(module)
+
+	for name, test in pairs(tests) do
+		local ok, err = pcall(test)
+
+		if not ok then
+			io.stderr:write(("FAIL %s/%s\n%s\n"):format(file, name, err))
+			os.exit(1)
+		end
+
+		print(("PASS %s/%s\n"):format(file, name))
+	end
+end
---
+++ tests/test-init.lua
@@ -0,0 +1,47 @@
+return {
+	test_apply = function()
+		-- Given
+		local theme = require("omarchy-theme")
+		local env = require("omarchy-theme.environment")
+		env.omarchy_current_theme_colors_path = "tests/current/theme/colors.toml"
+		env.omarchy_current_theme_name_path = "tests/current/theme.name"
+
+		-- When
+		theme.apply()
+
+		-- Then
+		local want = "#c2c2b0"
+		local got = string.format("#%06x", vim.api.nvim_get_hl(0, { name = "Normal" }).fg)
+		assert(got == want, "want:" .. want .. ",got:" .. got)
+	end,
+	test_apply_outside_of_omarchy = function()
+		-- Given
+		local want = "#ffffff"
+		vim.api.nvim_set_hl(0, "Normal", { fg = want })
+
+		local theme = require("omarchy-theme")
+		local env = require("omarchy-theme.environment")
+		env.omarchy_current_theme_colors_path = "tests/not-existing/theme/colors.toml"
+		env.omarchy_current_theme_name_path = "tests/not-existing/theme.name"
+		-- When
+		theme.apply()
+
+		-- Then
+		local got = string.format("#%06x", vim.api.nvim_get_hl(0, { name = "Normal" }).fg)
+		assert(got == want, "want:" .. want .. ",got:" .. got)
+	end,
+	test_apply_clears_old_highlights = function()
+		-- Given
+		vim.api.nvim_set_hl(0, "HighlightGroupToClear", { fg = "#ffffff" })
+
+		local theme = require("omarchy-theme")
+		local env = require("omarchy-theme.environment")
+		env.omarchy_current_theme_colors_path = "tests/current/theme/colors.toml"
+		env.omarchy_current_theme_name_path = "tests/current/theme.name"
+		-- When
+		theme.apply()
+
+		-- Then
+		assert(vim.api.nvim_get_hl(0, { name = "HighlightGroupToClear" }).fg == nil, "Highlights were not cleared")
+	end,
+}
