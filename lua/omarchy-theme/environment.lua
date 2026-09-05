b0e92fd7521ce3232d151efa144014c780e4339e
Antti Eskelinen <antti.eskelinen.dev@gmail.com>
2026-09-05 12:06:53 +0300

    refactor: remove unnecessary caching

--- lua/omarchy-theme/environment.lua
+++ lua/omarchy-theme/environment.lua
@@ -4,13 +4,8 @@ M.omarchy_current_theme_name_path = vim.fs.joinpath(vim.env.HOME, ".local", "sta
 M.omarchy_current_theme_colors_path =
 	vim.fs.joinpath(vim.env.HOME, ".local", "state", "omarchy", "current", "theme", "colors.toml")
 
-local is_omarchy = nil
 M.is_omarchy = function()
-	if is_omarchy == nil then
-		is_omarchy = vim.uv.fs_stat(M.omarchy_current_theme_name_path) ~= nil
-	end
-
-	return is_omarchy
+	return vim.uv.fs_stat(M.omarchy_current_theme_name_path) ~= nil
 end
 
 return M
